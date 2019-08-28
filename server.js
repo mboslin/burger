abdb3624b80bac8b74165ce8dcf5ea7b80e73f85
mboslin <michael@boslin.com>
2019-08-28 03:42:56 -0700

    Updated server.js connection.

--- server.js
+++ server.js
@@ -8,13 +8,10 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 //Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
 
-//Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//Parse application/json
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -29,5 +26,5 @@ app.use(routes);
 
 //App is listening...
 app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+  console.log("Server listening on: http://localhost:" + PORT);
 });
